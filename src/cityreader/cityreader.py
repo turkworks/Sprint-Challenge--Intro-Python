9c9a90056b62492c4b08236ff5e2c364718f6692
turkworks <andrewcengiz@gmail.com>
2019-03-29 09:27:58 -0600

    not able to complete this sprint

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -30,7 +30,7 @@ for c in cities:
     print(c)
 
 # STRETCH GOAL!
-#
+
 # Allow the user to input two points, each specified by latitude and longitude.
 # These points form the corners of a lat/lon square. Pass these latitude and 
 # longitude values as parameters to the `cityreader_stretch` function, along
