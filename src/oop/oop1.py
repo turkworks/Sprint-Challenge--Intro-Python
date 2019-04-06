49a00b9d0484a4b56711535bac9898f1199be437
turkworks <andrewcengiz@gmail.com>
2019-04-05 18:26:24 -0600

    fixed oop2.py and finished cityreader

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,72 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    # Base Class
+    def __init__(self):
+        print('Engine is running')
+
+
+Vehicle()
+
+
+class FlightVehicle(Vehicle):
+    # Child Class
+    def __init__(self):
+        Vehicle()
+        print('Wings have been inspected')
+
+
+FlightVehicle()
+
+
+class Airplane(FlightVehicle):
+    # Grandchild Class
+    def __init__(self):
+        FlightVehicle()
+        print("Airplane is cruising at 5,000 feet")
+
+
+Airplane()
+
+
+class Starship(FlightVehicle):
+    # Grandchild Class
+    def __init__(self):
+        FlightVehicle()
+        print('lightspeed engaged')
+
+
+Starship()
+
+
+class GroundVehicle(Vehicle):
+    # Child Class
+    def __init__(self):
+        Vehicle()
+        print('lights are on')
+
+
+GroundVehicle()
+
+
+class Car(GroundVehicle):
+    # Grandchild Class
+    def __init__(self):
+        GroundVehicle()
+        print('Seat belts are on')
+
+
+Car()
+
+
+class Motorcycle(GroundVehicle):
+    # Grandchild Class
+    def __init__(self):
+        GroundVehicle()
+        print('helmets are on')
+
+
+Motorcycle()
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,11 +3,22 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    sound = "vroooom"
+
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return self.sound
+
+
+class Motorcycle(GroundVehicle):
+    sound = 'BRAAAP!!'
+
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -16,9 +27,7 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
-
 # TODO
-
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -29,4 +38,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for vehicle in vehicles:
+    vehicle.drive()
